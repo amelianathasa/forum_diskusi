06d8211a438b3cc7b2142e289930ebc7392e0171
Amelia Nathasa <amelia.nathasa.tif421@polban.ac.id>
2024-04-13 09:46:07 +0700

    update tambah diskusi

--- src/components/custom/FormDialog.tsx
+++ src/components/custom/FormDialog.tsx
@@ -23,7 +23,7 @@ const Switch: React.FC<{ checked: boolean; onChange: (checked: boolean) => void 
 
   return (
     <div
-      className={`relative inline-block w-10 h-6 rounded-full transition-colors duration-200  focus:outline-none ${checked ? 'bg-green-500' : 'bg-gray-300'}`}
+      className={`relative inline-block w-10 h-6 rounded-full transition-colors duration-200 focus:outline-none ${checked ? 'bg-green-500' : 'bg-gray-300'}`}
       onClick={handleClick}
       role="switch"
       aria-checked={checked}
@@ -37,21 +37,35 @@ const Switch: React.FC<{ checked: boolean; onChange: (checked: boolean) => void 
 
 const FormDialog: React.FC<FormDialogProps> = ({ isOpen, onClose }) => {
   const inputRef = useRef<HTMLInputElement>(null);
-  const [open, setOpen] = useState(false);
   const [tags, setTags] = useState<Tag[]>([]);
-  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const [selectedTags, setSelectedTags] = useState<{ [key: number]: boolean }>({});
   const [inputValue, setInputValue] = useState("");
   const [anonymousMode, setAnonymousMode] = useState(false);
   const [contentLength, setContentLength] = useState(0);
+  const [formData, setFormData] = useState({
+    title: "",
+    content: "",
+    tags: [] as number[], // Array to store tag IDs
+    user_id: "user1011",
+    author: "Adilla",
+  });
   const quillRef = useRef<ReactQuill>(null);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [isSwitchOpen, setIsSwitchOpen] = useState(false);
+  const [isFormValid, setIsFormValid] = useState(false);
 
   useEffect(() => {
     fetchTags();
   }, []);
 
+  useEffect(() => {
+    // Check form validity whenever formData, tags, or selectedTags change
+    checkFormValidity();
+  }, [formData, tags, selectedTags]);
+
   const fetchTags = async () => {
     try {
-      const response = await fetch("http://localhost:3000/discussion/discussion/tags");
+      const response = await fetch("http://localhost:3000/discussion/discussion/discussion/discussion/tags");
       if (!response.ok) {
         throw new Error("Failed to fetch tags");
       }
@@ -63,7 +77,11 @@ const FormDialog: React.FC<FormDialogProps> = ({ isOpen, onClose }) => {
   };
 
   const handleUnselect = useCallback((tag: Tag) => {
-    setSelectedTags((prev) => prev.filter((t) => t.id !== tag.id));
+    setSelectedTags((prev) => {
+      const updatedTags = { ...prev };
+      delete updatedTags[tag.id];
+      return updatedTags;
+    });
   }, []);
 
   const handleKeyDown = useCallback(
@@ -72,11 +90,14 @@ const FormDialog: React.FC<FormDialogProps> = ({ isOpen, onClose }) => {
       if (input) {
         if (e.key === "Delete" || e.key === "Backspace") {
           if (input.value === "") {
-            setSelectedTags((prev) => {
-              const newSelectedTags = [...prev];
-              newSelectedTags.pop();
-              return newSelectedTags;
-            });
+            const lastTag = Object.keys(selectedTags).pop();
+            if (lastTag) {
+              setSelectedTags((prev) => {
+                const updatedTags = { ...prev };
+                delete updatedTags[parseInt(lastTag)];
+                return updatedTags;
+              });
+            }
           }
         }
         if (e.key === "Escape") {
@@ -84,38 +105,108 @@ const FormDialog: React.FC<FormDialogProps> = ({ isOpen, onClose }) => {
         }
       }
     },
-    []
+    [selectedTags]
   );
 
-  const selectables = tags.filter((tag) => !selectedTags.some((selectedTag) => selectedTag.id === tag.id));
+  const selectables = tags.filter((tag) => !selectedTags[tag.id]);
 
   const handleSelectTag = (tag: Tag) => {
+    setSelectedTags((prev) => ({
+      ...prev,
+      [tag.id]: true,
+    }));
+    
+    setFormData((prev) => ({
+      ...prev,
+      tags: [...prev.tags, tag.id], // Menambahkan id tag yang dipilih ke formData.tags
+    }));
+
     setInputValue("");
-    setSelectedTags((prev) => [...prev, tag]);
+    setDropdownOpen(false);
   };
 
   const handleToggleAnonymousMode = () => {
-    setAnonymousMode((prev) => {
-      console.log(!prev); // Log nilai baru setelah diubah
-      return !prev;
-    });
+    setAnonymousMode((prev) => !prev);
+    setIsSwitchOpen(false);
   };
 
   const handleContentChange = (content: string) => {
-    // Menghitung jumlah karakter dari konten
-    const text = content.replace(/<[^>]*>/g, "");
-    if (text.length <= 500) {
-      setContentLength(text.length);
-    } else {
-      // Menghapus karakter yang melebihi 500 jika ada
-      const delta = text.length - 500;
-      const quill = quillRef.current?.getEditor();
+    const text = content.replace(/(<([^>]+)>)/gi, "");
+    setContentLength(text.length);
+
+    // Hentikan pengetikan jika mencapai 500 karakter
+    if (text.length >= 500) {
+      const quill = quillRef.current;
       if (quill) {
-        quill.deleteText(500, delta);
+        quill.blur();
       }
     }
   };
 
+  const handleTagInputClick = () => {
+    setDropdownOpen(true);
+    setIsSwitchOpen(false);
+  };
+
+  const handleTagInputBlur = () => {
+    // Menambahkan delay agar dropdown tidak tertutup saat mengklik pada tag
+    setTimeout(() => {
+      setDropdownOpen(false);
+    }, 200);
+  };
+
+  const handleSwitchBlur = () => {
+    setIsSwitchOpen(false);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  
+    try {
+      // Validate form before submitting
+      if (!isFormValid) {
+        throw new Error("Form is not valid");
+      }
+  
+      // Create the new thread with tagIds
+      const response = await fetch("http://localhost:3000/discussion/thread", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: formData.title,
+          content: formData.content,
+          anonymous: anonymousMode,
+          tags: formData.tags, // Mengirim ID tag ke backend
+          user_id: formData.user_id,
+          author: formData.author,
+        }),
+      });
+  
+      if (!response.ok) {
+        throw new Error("Failed to create thread");
+      }
+  
+      const data = await response.json();
+  
+      console.log("Thread created:", data);
+  
+      onClose(); // Tutup dialog setelah berhasil membuat thread
+    } catch (error) {
+      console.error("Error creating thread:", error);
+      // Handle error, optionally provide feedback to the user
+      alert("Failed to create thread. Please try again.");
+    }
+  };
+
+  const checkFormValidity = () => {
+    const isTitleValid = formData.title.trim() !== "";
+    const isContentValid = formData.content.trim().length >= 1; // Content minimal 1 karakter
+    const isTagsValid = formData.tags.length > 0; // Minimal 1 tag dipilih
+    setIsFormValid(isTitleValid && isContentValid && isTagsValid);
+  };
+  
   return (
     <>
       {isOpen && (
@@ -123,13 +214,13 @@ const FormDialog: React.FC<FormDialogProps> = ({ isOpen, onClose }) => {
           <div className="absolute inset-0 bg-gray-900 opacity-50" onClick={onClose}></div>
           <div className="bg-white p-6 rounded-lg shadow-xl z-10" style={{ maxWidth: "1000px", width: "90%", maxHeight: "100vh", overflowY: "auto" }}>
             <h2 className="text-lg font-bold mb-4">Buat Pertanyaan Baru</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className="flex flex-col space-y-4">
                 <div>
                   <label htmlFor="title" className="block text-sm font-medium text-gray-700 mb-3 ml-1">
                     Judul Pertanyaan
                   </label>
-                  <Input id="title" type="text" />
+                  <Input id="title" type="text" value={formData.title} onChange={(e) => setFormData({ ...formData, title: e.target.value })} />
                   <label htmlFor="title" className="block text-sm font-small text-gray-400 mt-1 ml-1" style={{ fontSize: "12px" }}>
                     Buat judul dengan spesifik sesuai dengan permasalahan Anda.
                   </label>
@@ -140,7 +231,11 @@ const FormDialog: React.FC<FormDialogProps> = ({ isOpen, onClose }) => {
                   </label>
                   <ReactQuill
                     id="content"
-                    onChange={handleContentChange}
+                    value={formData.content}
+                    onChange={(value) => {
+                      setFormData({ ...formData, content: value });
+                      handleContentChange(value);
+                    }}
                     modules={{
                       toolbar: [
                         [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -169,6 +264,7 @@ const FormDialog: React.FC<FormDialogProps> = ({ isOpen, onClose }) => {
                       "code-block",
                     ]}
                     ref={quillRef}
+                    readOnly={contentLength >= 500} // Konten tidak bisa di-edit jika sudah mencapai 500 karakter
                   />
                   <label htmlFor="content" className="block text-sm font-small text-gray-400 mt-1 ml-1" style={{ fontSize: "12px" }}>
                     Uraikan pertanyaan anda disini. Anda juga dapat menambahkan kode, gambar dan lainnya untuk memperjelas pertanyaan. Min. 500 karakter. ({contentLength} / 500)
@@ -178,32 +274,37 @@ const FormDialog: React.FC<FormDialogProps> = ({ isOpen, onClose }) => {
                   <label htmlFor="tags" className="block text-sm font-medium text-gray-700 mb-3 ml-1">
                     Tags
                   </label>
-                  <div className="relative mt-2">
+                  <div className="relative mt-2" onBlur={handleTagInputBlur}>
                     <div className="flex flex-wrap gap-2">
-                      {selectedTags.map((tag) => (
-                        <div
-                          key={tag.id}
-                          className="flex items-center bg-gray-200 rounded-md p-2"
-                        >
-                          <span>{tag.name}</span>
-                          <button
-                            className="ml-1 outline-none focus:ring-2 focus:ring-offset-2"
-                            onClick={() => handleUnselect(tag)}
+                      {Object.keys(selectedTags).map((tagId) => {
+                        const tag = tags.find((t) => t.id === parseInt(tagId));
+                        if (!tag) return null;
+                        return (
+                          <div
+                            key={tag.id}
+                            className="flex items-center bg-gray-200 rounded-md p-2"
                           >
-                            <X className="h-4 w-4 text-gray-500 hover:text-gray-700" />
-                          </button>
-                        </div>
-                      ))}
+                            <span>{tag.name}</span>
+                            <button
+                              className="ml-1 outline-none focus:ring-2 focus:ring-offset-2"
+                              onClick={() => handleUnselect(tag)}
+                            >
+                              <X className="h-4 w-4 text-gray-500 hover:text-gray-700" />
+                            </button>
+                          </div>
+                        );
+                      })}
                       <input
                         ref={inputRef}
                         value={inputValue}
+                        onClick={handleTagInputClick}
                         onChange={(e) => setInputValue(e.target.value)}
                         onKeyDown={handleKeyDown}
                         placeholder="Select tags..."
                         className="ml-2 bg-transparent outline-none placeholder-gray-500 flex-1 border border-gray-300 rounded-md px-3 py-2"
                       />
                     </div>
-                    {open && selectables.length > 0 && (
+                    {dropdownOpen && selectables.length > 0 && (
                       <div className="absolute w-full z-10 top-full rounded-md border bg-white shadow-md outline-none animate-in">
                         <div
                           className="p-2 space-y-2"
@@ -229,10 +330,15 @@ const FormDialog: React.FC<FormDialogProps> = ({ isOpen, onClose }) => {
               </div>
               <div className="flex items-center justify-between mt-4">
                 <div className="flex items-center space-x-2">
-                  <Switch checked={anonymousMode} onChange={handleToggleAnonymousMode} />
-                  <Label htmlFor="anonymous-mode">Anonymous Mode</Label>
+                  <div className="relative">
+                    <Switch checked={anonymousMode} onChange={handleToggleAnonymousMode} />
+                    <Label htmlFor="anonymous-mode">Anonymous Mode</Label>
+                    {dropdownOpen && (
+                      <div className="absolute top-full left-0 z-10 w-10 h-6 bg-transparent" onClick={handleSwitchBlur}></div>
+                    )}
+                  </div>
                 </div>
-                <Button type="submit" className="bg-[#38B0AB] hover:bg-teal-700 text-gray-100 px-4 py-2 rounded-md">
+                <Button type="submit" className={`bg-[#38B0AB] hover:bg-teal-700 text-gray-100 px-4 py-2 rounded-md ${!isFormValid ? 'opacity-50 cursor-not-allowed' : ''}`} disabled={!isFormValid}>
                   Buat Pertanyaan
                 </Button>
               </div>
