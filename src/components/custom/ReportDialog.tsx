8720f252ca8fb0f7a8000310fa7cc60cc1301c4e
regiprnm432 <regi.purnama.tif421@polban.ac.id>
2024-04-14 15:54:15 +0700

    Fix error in select option functionality in report dialog

--- src/components/custom/ReportDialog.tsx
+++ src/components/custom/ReportDialog.tsx
@@ -1,4 +1,7 @@
 import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
 import { Button } from "@/components/ui/button"
 import {
   Dialog,
@@ -8,8 +11,16 @@ import {
   DialogHeader,
   DialogTitle
 } from "@/components/ui/dialog"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  FormDescription
+} from "@/components/ui/form"
 import { ScrollArea } from "@/components/ui/scroll-area"
-import { Label } from "@/components/ui/label"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 
 interface ReportDialogProps {
@@ -24,25 +35,71 @@ interface ReportType{
   description: string;
 }
 
-const ReportDialog =({
-  isDialogOpen, 
-  setIsDialogOpen,
-  commentId 
-   }: ReportDialogProps,
-  ) => {
-  const [commentData, setCommentData] = useState(null); // State untuk menyimpan data komentar
-  const [selectedReportType, setSelectedReportType] = useState("spam");
+const reportTypes: ReportType[] = [
+  {
+    value: "spam",
+    label: "Spam",
+    description: "Akun palsu, penipuan keuangan, memposting tautan berbahaya, menyalahgunakan hastag, keterlibatan palsu, balasan berulang, posting ulang, atau direct message"
+  },
+  {
+    value: "privasi",
+    label: "Privasi",
+    description: "Membagikan informasi pribadi, mengancam akan membagikan/menyebarkan informasi pribadi, membagikan gambar intim tanpa persetujuan, membagikan gambar saya yang tidak saya kehendaki di platform ini"
+  },
+  {
+    value: "kebencian",
+    label: "Kebencian",
+    description: "Cercaan, stereotip rasis atau seksis, dehumanisasi, menyulut ketakutan atau diskriminasi, referensi kebencian, simbol & logo kebencian"
+  },
+  {
+    value: "penghinaan",
+    label: "Penghinaan & Pelecehan secara Online",
+    description: "Penghinaan, konten seksual yang tidak diinginkan & objektifikasi grafis, konten NSFW & grafis yang tidak diinginkan, penyangkalan peristiwa kekerasan, pelecehan bertarget dan memprovokasi pelecehan"
+  },
+  {
+    value: "kekerasan",
+    label: "Tutur Kekerasan",
+    description: "Ancaman kekerasan, berharap terjadinya celaka, mengagungkan kekerasan, penghasutan kekerasan, penghasutan kekerasan dengan kode"
+  },
+  {
+    value: "media-sensitif",
+    label: "Media yang sensitif atau mengganggu",
+    description: "Graphic content, gratutious gore, adult nudity & sexual behavior, violent sexual conduct, bestiality & necrophilia, media depicting a decreased individual"
+  },
+  {
+    value: "bunuh-diri",
+    label: "Bunuh diri atau melukai diri sendiri",
+    description: "Mendorong, mempromosikan, memberikan intruksi, atau membagikan metode untuk melukai diri"
+  }
+];
+
+// Skema form
+const formSchema = z.object({
+  type: z.enum(["", ...reportTypes.map(reportType => reportType.value)])
+});
+
+function ReportDialog({
+  isDialogOpen, setIsDialogOpen, commentId
+}: ReportDialogProps) {
+  const [commentData, setCommentData] = useState<any>(null);
   const [userId, setUserId] = useState<string>("");
   const [threadId, setThreadId] = useState<number>(0);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      type: reportTypes[0].value,
+    },
+  });
 
   const fetchCommentData = async () => {
     try {
       const response = await fetch(
         `http://localhost:3000/discussion/comment/${commentId}`
-        );
-        if (!response.ok) {
-          throw new Error('Failed to fetch comment data');
-        }
+      );
+      if (!response.ok) {
+        throw new Error('Failed to fetch comment data');
+      }
       const data = await response.json();
       setCommentData(data); // Set data komentar ke state
       setUserId(data.user_id); // Set user_id ke state
@@ -53,9 +110,6 @@ const ReportDialog =({
     }
   };
 
-  const handleReportTypeChange = (value: string) => {
-    setSelectedReportType(value);
-  };
 
   const handleReportSubmit = async () => {
     try {
@@ -63,11 +117,11 @@ const ReportDialog =({
         user_id: userId,
         thread_id: threadId,
         comment_id: commentId,
-        report_type: selectedReportType,
+        report_type: formSchema.parse({ type: form.watch('type') }).type,
         created_at: new Date().toISOString(),
         status_review: false
       };
-  
+
       const response = await fetch('http://localhost:3000/discussion/report', {
         method: 'POST',
         headers: {
@@ -75,57 +129,18 @@ const ReportDialog =({
         },
         body: JSON.stringify(reportData)
       });
-  
+
       if (!response.ok) {
         throw new Error('Failed to submit report');
       }
 
-      console.log('Report data successfully submitted:', reportData); 
+      console.log('Report data successfully submitted:', reportData);
 
       setIsDialogOpen(false);
     } catch (error) {
       console.error('Error reporting:', error);
     }
   };
-  
-
-  const reportTypes: ReportType[] = [
-    {
-      value: "spam",
-      label: "Spam",
-      description: "Akun palsu, penipuan keuangan, memposting tautan berbahaya, menyalahgunakan hastag, keterlibatan palsu, balasan berulang, posting ulang, atau direct message"
-    },
-    {
-      value: "privasi",
-      label: "Privasi",
-      description: "Membagikan informasi pribadi, mengancam akan membagikan/menyebarkan informasi pribadi, membagikan gambar intim tanpa persetujuan, membagikan gambar saya yang tidak saya kehendaki di platform ini"
-    },
-    {
-      value: "kebencian",
-      label: "Kebencian",
-      description: "Cercaan, stereotip rasis atau seksis, dehumanisasi, menyulut ketakutan atau diskriminasi, referensi kebencian, simbol & logo kebencian"
-    },
-    {
-      value: "penghinaan",
-      label: "Penghinaan & Pelecehan secara Online",
-      description: "Penghinaan, konten seksual yang tidak diinginkan & objektifikasi grafis, konten NSFW & grafis yang tidak diinginkan, penyangkalan peristiwa kekerasan, pelecehan bertarget dan memprovokasi pelecehan"
-    },
-    {
-      value: "kekerasan",
-      label: "Tutur Kekerasan",
-      description: "Ancaman kekerasan, berharap terjadinya celaka, mengagungkan kekerasan, penghasutan kekerasan, penghasutan kekerasan dengan kode"
-    },
-    {
-      value: "media-sensitif",
-      label: "Media yang sensitif atau mengganggu",
-      description: "Graphic content, gratutious gore, adult nudity & sexual behavior, violent sexual conduct, bestiality & necrophilia, media depicting a decreased individual"
-    },
-    {
-      value: "bunuh-diri",
-      label: "Bunuh diri atau melukai diri sendiri",
-      description: "Mendorong, mempromosikan, memberikan intruksi, atau membagikan metode untuk melukai diri"
-    }
-  ];
 
   // Mengambil data komentar saat dialog dibuka
   useEffect(() => {
@@ -143,28 +158,41 @@ const ReportDialog =({
             Apa Jenis Masalah yang Anda Laporkan?
           </DialogDescription>
         </DialogHeader>
-          <ScrollArea className="h-[350px] w-[350px] rounded-md border p-4 bg-slate-50">
-          <RadioGroup>
-            <div className="flex flex-col space-y-4">
-              {reportTypes.map((type) => (
-                <div key={type.value} className="flex items-center space-x-2">
-                  <RadioGroupItem 
-                    value={type.value} 
-                    id={`r${type.value}`}
-                    checked={selectedReportType === type.value}
-                    onChange={() => handleReportTypeChange(type.value)}
-                  />
-                  <div className="flex flex-col">
-                    <Label htmlFor={`r${type.value}`}>{type.label}</Label>
-                    <p className="text-sm font-extralight">{type.description}</p>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </RadioGroup>
-          </ScrollArea>
-        <DialogFooter>
-          <Button type="submit" onClick={handleReportSubmit} className="bg-[#38B0AB] hover:bg-teal-700">Laporkan</Button>
+        <ScrollArea className="h-[350px] w-[350px] rounded-md border p-4 bg-slate-50">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleReportSubmit)} className="space-y-8">
+                <FormField
+                  control={form.control}
+                  name="type"
+                  render={({ field }) => (
+                    <FormItem className="space-y-3">
+                      <FormControl>
+                        <RadioGroup
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                          className="flex flex-col space-y-1"
+                        >
+                          {reportTypes.map((reportType) => (
+                            <FormItem key={reportType.value} className="flex items-center space-x-3 space-y-0">
+                              <FormControl>
+                                <RadioGroupItem value={reportType.value} />
+                              </FormControl>
+                              <div>
+                                <FormLabel>{reportType.label}</FormLabel>
+                                <FormDescription>{reportType.description}</FormDescription>
+                              </div>
+                            </FormItem>
+                          ))}
+                        </RadioGroup>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )} />
+                  <Button type="submit" className="bg-[#38B0AB] hover:bg-teal-700 fixed bottom-4 right-[25px]">Laporkan</Button>
+            </form>
+          </Form>
+        </ScrollArea>
+        <DialogFooter className="h-8">
         </DialogFooter>
       </DialogContent>
     </Dialog>
